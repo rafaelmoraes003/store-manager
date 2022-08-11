47b22831e4133cd1fbed0ebf86171102da6d3f33
rafael moraes <rafaelsm003@gmail.com>
2022-08-11 18:47:05 -0300

    feat: cria validação por nome para criação de produtos

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productsController = require('../controllers/productsController');
+const { validateName } = require('../middlewares/validateName');
 
 const products = express.Router();
 
 products.get('/', productsController.getAll);
 products.get('/:id', productsController.getById);
-products.post('/', productsController.create);
+products.post('/', validateName, productsController.create);
 
 module.exports = products;
