b68491df413db89376b8ce15ab4cb4f9c82a0e37
rafael moraes <rafaelsm003@gmail.com>
2022-08-12 20:51:46 -0300

    feat: cria endpoint products/search?q=searchTerm

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,7 @@ const { validateName } = require('../middlewares/validators/validateName');
 const products = express.Router();
 
 products.get('/', productsController.getAll);
+products.get('/search', productsController.getByName);
 products.get('/:id', productsController.getById);
 products.delete('/:id', productsController.exclude);
 
