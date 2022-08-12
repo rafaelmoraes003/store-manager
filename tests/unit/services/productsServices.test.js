c7532c9c851bd6c9ea9e5126f20bae1ce0511c14
rafael moraes <rafaelsm003@gmail.com>
2022-08-12 19:32:26 -0300

    test: criação de testes de atualização de produto da camada service

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -95,4 +95,50 @@ describe('Testa a camada service de products', () => {
     });
 
   });
+
+  describe('Testa a atualização de um produto', () => {
+    describe('Testa em caso de sucesso', () => {
+
+      before(async () => {
+        sinon.stub(productsModel, 'getById').resolves({
+          true: true,
+        });
+
+        sinon.stub(productsModel, 'update').resolves({
+          id: 1,
+          name: 'Produto #01',
+        });
+      });
+
+      after(async () => {
+        productsModel.getById.restore();
+        productsModel.update.restore();
+      });
+
+      it('Verifica se retona um objeto com duas chaves', async () => {
+        const response = await productsService.update(1, 'Produto #01');
+        expect(response).to.be.an('object');
+        expect(response).to.have.keys('code', 'data');
+      });
+
+    });
+
+    describe('Testa em caso de falha', () => {
+
+      before(async () => {
+        sinon.stub(productsModel, 'getById').resolves(undefined);
+      });
+
+      after(async () => {
+        productsModel.getById.restore();
+      });
+
+      it('Verifica se retona um objeto de erro', async () => {
+        const response = await productsService.update(1, 'Produto #01');
+        expect(response).to.be.an('object');
+        expect(response).to.have.key('error');
+      });
+
+    });
+  });
 });
