96bc4da24c03cd46b5d98b88c5256c584d7eca4b
rafael moraes <rafaelsm003@gmail.com>
2022-08-16 15:49:35 -0300

    test: cria cobertura de testes de criação, exclusão e atualização de vendas da camada service

--- tests/unit/services/salesServices.test.js
+++ tests/unit/services/salesServices.test.js
@@ -53,4 +53,121 @@ describe('Testa a camada service de sales', () => {
 
     });
   });
+
+  describe('Testa a criação de uma venda', () => {
+
+    before(async () => {
+      sinon.stub(salesModel, 'createSale').resolves({
+        id: 3,
+      });
+
+      sinon.stub(Promise, 'all').resolves();
+    });
+
+    after(async () => {
+      salesModel.createSale.restore();
+      Promise.all.restore();
+    });
+
+    it('Verifica se retorna um objeto', async () => {
+      const response = await salesService.createSale([{
+        productId: 2,
+        quantity: 7,
+      }]);
+
+      expect(response).to.be.an('object');
+      expect(response).to.have.keys('code', 'data');
+    });
+
+  });
+
+  describe('Testa a exclusão de uma venda', () => {
+    describe('Testa em caso de sucesso', () => {
+
+      before(async () => {
+        sinon.stub(salesModel, 'getById').resolves([{}]);
+
+        sinon.stub(salesModel, 'deleteSale').resolves();
+      });
+
+      after(async () => {
+        salesModel.getById.restore();
+        salesModel.deleteSale.restore();
+      });
+
+      it('Verifica se retorna um objeto', async () => {
+        const response = await salesService.deleteSale(3);
+
+        expect(response).to.be.an('object');
+        expect(response).to.have.keys('code');
+      });
+
+    });
+
+    describe('Testa em caso de falha', () => {
+
+      before(async () => {
+        sinon.stub(salesModel, 'getById').resolves([]);
+      });
+
+      after(async () => {
+        salesModel.getById.restore();
+      });
+
+      it('Verifica se retorna um objeto', async () => {
+        const response = await salesService.deleteSale(99);
+
+        expect(response).to.be.an('object');
+        expect(response).to.have.key('error');
+      });
+
+    });
+  });
+
+  describe('Testa a atualização de uma venda', () => {
+    describe('Testa em caso de sucesso', () => {
+
+      before(async () => {
+        sinon.stub(salesModel, 'getById').resolves([{}]);
+        sinon.stub(salesModel, 'deleteSalesProducts').resolves();
+        sinon.stub(Promise, 'all').resolves();
+      });
+
+      after(async () => {
+        salesModel.getById.restore();
+        salesModel.deleteSalesProducts.restore();
+        Promise.all.restore();
+      });
+
+      it('Verifica se retorna um objeto', async () => {
+        const response = await salesService.updateSale(2, [{
+          productId: 4,
+          quantity: 2
+        }]);
+
+        expect(response).to.be.an('object');
+        expect(response).to.have.keys('code', 'data');
+      });
+
+    });
+
+    describe('Testa em caso de falha', () => {
+
+      before(async () => {
+        sinon.stub(salesModel, 'getById').resolves([]);
+      });
+
+      after(async () => {
+        salesModel.getById.restore();
+      });
+
+      it('Verifica se retorna um objeto de erro', async () => {
+        const response = await salesService.updateSale(99);
+
+        expect(response).to.be.an('object');
+        expect(response).to.have.key('error');
+      });
+
+    });
+  });
 });
