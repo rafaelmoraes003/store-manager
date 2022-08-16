6740f773e04e4f8b3ce8ed36ff4b703f53e26e62
rafael moraes <rafaelsm003@gmail.com>
2022-08-16 14:47:28 -0300

    feat: cria endpoint para validar e cadastrar vendas

--- routes/sales.js
+++ routes/sales.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const salesController = require('../controllers/salesController');
+const validateProductId = require('../middlewares/validators/validateProductId');
+const validateQuantity = require('../middlewares/validators/validateQuantity');
 
 const sales = express.Router();
 
 sales.get('/', salesController.getAll);
 sales.get('/:id', salesController.getById);
+// sales.delete('/:id', salesController.deleteSale);
+
+sales.use(
+  validateProductId.validateProductId,
+  validateProductId.validateProductIdExistence,
+  validateQuantity.validateQuantity,
+  validateQuantity.validateQuantityValue,
+);
+
+sales.post('/', salesController.createSale);
+// sales.put('/:id', salesController.updateSale);
 
 module.exports = sales;
